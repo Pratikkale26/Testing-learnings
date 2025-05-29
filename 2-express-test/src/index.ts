5f07a8b3469ea7568043779262e4dfb5fb93abaf
Pratik Kale <147796613+PKBOSS26@users.noreply.github.com>
2025-05-29 12:52:04 +0530

    express with zod and headers test

--- 2-express-test/src/index.ts
+++ 2-express-test/src/index.ts
@@ -1,16 +1,65 @@
+// import express from "express";
+
+// export const app = express();
+// app.use(express.json());
+
+// app.post("/sum", (req, res) => {
+//     const a = req.body.a;
+//     const b = req.body.b;
+//     const ans = a + b;
+
+//     res.json({
+//         ans
+//     })
+// });
+
+// // we wont add "app.listen()" here. => bin.ts file
+
+
+
 import express from "express";
+import { z } from "zod";
 
 export const app = express();
 app.use(express.json());
 
+const sumInput = z.object({
+    a: z.number(),
+    b: z.number()
+})
+
 app.post("/sum", (req, res) => {
-    const a = req.body.a;
-    const b = req.body.b;
-    const ans = a + b;
+    const parsedResponse = sumInput.safeParse(req.body)
+    if (!parsedResponse.success) {
+        res.status(411).json({
+            message: "Incorrect inputs"
+        })
+        return
+    }
+
+    const ans = parsedResponse.data.a + parsedResponse.data.b;
 
     res.json({
         ans
     })
 });
 
-// we wont add "app.listen()" here. => bin.ts file
+app.get("/sum", (req, res) => {
+    const parsedResponse = sumInput.safeParse({
+        a: Number(req.headers["a"]),
+        b: Number(req.headers["b"])
+    })
+
+    if (!parsedResponse.success) {
+        res.status(411).json({
+            message: "Incorrect inputs"
+        })
+        return
+    }
+
+    const ans = parsedResponse.data.a + parsedResponse.data.b;
+
+    res.json({
+        ans
+    })
+});
--- 2-express-test/src/tests/index.test.ts
+++ 2-express-test/src/tests/index.test.ts
@@ -39,4 +39,29 @@ describe("POST /sum", () => {
         expect(res.body.ans).toBe(0);
     })
 
+    it("should return 411 if no inputs are provided", async () => {
+      const res = await request(app).post("/sum").send({});
+      expect(res.statusCode).toBe(411);
+      expect(res.body.message).toBe("Incorrect inputs");
+    });
+});
+
+
+describe("GET /sum", () => {
+  it("should return the sum of two numbers", async () => {
+      const res = await request(app).get("/sum").set({
+          a: "1",
+          b: "2"
+        }).send();
+
+      expect(res.statusCode).toBe(200);
+      expect(res.body.ans).toBe(3);
+  });
+
+  it("should return 411 if no inputs are provided", async () => {
+    const res = await request(app).get("/sum").send();
+
+    expect(res.statusCode).toBe(411);
+  });
+
 });
