6d8b5ef9bcdd768b811cb3577841364631397c4c
Pratik Kale <147796613+PKBOSS26@users.noreply.github.com>
2025-05-29 13:34:15 +0530

    moved jest to vitest

---
+++ 3-express-with-vitest/src/index.ts
@@ -0,0 +1,47 @@
+
+import express from "express";
+import { z } from "zod";
+
+export const app = express();
+app.use(express.json());
+
+const sumInput = z.object({
+    a: z.number(),
+    b: z.number()
+})
+
+app.post("/sum", (req, res) => {
+    const parsedResponse = sumInput.safeParse(req.body)
+    if (!parsedResponse.success) {
+        res.status(411).json({
+            message: "Incorrect inputs"
+        })
+        return
+    }
+
+    const ans = parsedResponse.data.a + parsedResponse.data.b;
+
+    res.json({
+        ans
+    })
+});
+
+app.get("/sum", (req, res) => {
+    const parsedResponse = sumInput.safeParse({
+        a: Number(req.headers["a"]),
+        b: Number(req.headers["b"])
+    })
+
+    if (!parsedResponse.success) {
+        res.status(411).json({
+            message: "Incorrect inputs"
+        })
+        return
+    }
+
+    const ans = parsedResponse.data.a + parsedResponse.data.b;
+
+    res.json({
+        ans
+    })
+});
---
+++ 3-express-with-vitest/src/tests/index.test.ts
@@ -0,0 +1,67 @@
+import {describe, expect, test, it} from 'vitest';
+import request from "supertest";
+import { app } from ".."
+
+describe("POST /sum", () => {
+    it("should return the sum of two numbers", async () => {
+        const res = await request(app).post("/sum").send({
+          a: 1,
+          b: 2
+        });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.ans).toBe(3);
+      });
+
+    it("should return the sum of two -ve numbers", async () => {
+        const res = await request(app).post("/sum").send({
+            a: -4,
+            b: -2
+        });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.ans).toBe(-6);
+    })
+
+    it("should return the sum of -ve and +ve numbers", async () => {
+        const res = await request(app).post("/sum").send({
+            a: -4,
+            b: +3
+        });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.ans).toBe(-1);
+    })
+
+    it("should return the sum of two 0", async () => {
+        const res = await request(app).post("/sum").send({
+            a: 0,
+            b: 0
+        });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.ans).toBe(0);
+    })
+
+    it("should return 411 if no inputs are provided", async () => {
+      const res = await request(app).post("/sum").send({});
+      expect(res.statusCode).toBe(411);
+      expect(res.body.message).toBe("Incorrect inputs");
+    });
+});
+
+
+describe("GET /sum", () => {
+  it("should return the sum of two numbers", async () => {
+      const res = await request(app).get("/sum").set({
+          a: "1",
+          b: "2"
+        }).send();
+
+      expect(res.statusCode).toBe(200);
+      expect(res.body.ans).toBe(3);
+  });
+
+  it("should return 411 if no inputs are provided", async () => {
+    const res = await request(app).get("/sum").send();
+
+    expect(res.statusCode).toBe(411);
+  });
+
+});
